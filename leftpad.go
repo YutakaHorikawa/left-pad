3d686231267b51443745e1bd388523bc2d029ccf
YutakaHorikawa <gozutakemaru@gmail.com>
2016-05-30 21:38:07 +0900

    add left-pad

---
+++ leftpad.go
@@ -0,0 +1,23 @@
+package leftpad
+
+import (
+	"bytes"
+	"unicode/utf8"
+)
+
+func Leftpad(str string, len int, ch string) string {
+	strlen := utf8.RuneCountInString(str)
+
+	if len < strlen {
+		return string([]rune(str)[0:len])
+	}
+
+	var buffer bytes.Buffer
+
+	diff := len - strlen
+	for i := 0; i < diff; i++ {
+		buffer.WriteString(ch)
+	}
+
+	return buffer.String() + str
+}
